49573899dfa29890463a4406e66795e870439842
Nathan Witmer <nathan@zerowidth.com>
2017-09-27 10:07:55 -0600

    Update install script to support non-synced Alfred prefs

--- install.sh
+++ install.sh
@@ -3,7 +3,18 @@
 set -e
 
 prefs=~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences-3.plist
-syncfolder=$(/usr/libexec/PlistBuddy -c "print :syncfolder" $prefs)
+syncfolder=$(/usr/libexec/PlistBuddy -c "print :syncfolder" $prefs 2>/dev/null || echo)
+if [ -z "$syncfolder" ]; then
+  syncfolder=~/Library/Application\ Support/Alfred\ 3
+fi
+
 workflows=$syncfolder/Alfred.alfredpreferences/workflows
 workflows=${workflows/\~/$HOME} # expand ~/
-ln -s "$(pwd)" "$workflows"
+
+if [ -d "$workflows" ]; then
+  echo "symlinking $(pwd) into $workflows"
+  ln -s "$(pwd)" "$workflows"
+else
+  echo "Could not find workflows folder: $workflows"
+  exit 1
+fi
