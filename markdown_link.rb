99af144e2f347bc7c56054fdc1577ce5252b4b64
Nathan Witmer <nathan@zerowidth.com>
2019-05-03 17:46:55 -0600

    Record latest snapshot of markdown_link.rb before deletion

--- markdown_link.rb
+++ markdown_link.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
 url = ARGV[0]
-if %r(github\.com/(?<user>[^/]+)/(?<repo>[^/]+)/(issues|pull)/(?<id>\d+)(#(?<anchor>.*))?) =~ url
-  without_anchor = url.split("#",2).first
+if %r(https://github\.com/(?<user>[^/]+)/(?<repo>[^/]+)/(issues|pull)/(?<id>\d+)(#(?<anchor>.*))?) =~ url
+  without_anchor = $&.split("#",2).first
   print "[%s/%s#%s](%s)" % [user, repo, id, without_anchor]
+elsif %r(https://github\.com/orgs/(?<org>[^/]+)/teams/(?<team>[^/]+)/discussions/(?<id>\d+)(#(?<anchor>.*))?) =~ url
+  without_anchor = $&.split("#",2).first
+  print "[@%s/%s#%s](%s)" % [org, team, id, without_anchor]
 else
   print url
 end
